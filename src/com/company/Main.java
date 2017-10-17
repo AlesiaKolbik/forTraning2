cbe7f6239af975992e92743320b2ea653ad3c339
alesya.kolbik <moyaalesia@mail.ru>
2017-10-17 17:04:58 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            int x = (int) (Math.random() * 100);
+            int s = 0;
+            if (x % 2 == 0)
+                {
+                    s += x;
+                    {
+                     System.out.println("s=" + s);}
+                }
+            }
+
+        }
+    }
+
+
